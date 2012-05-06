097dbae4223d133acb6205a3ae6f290685d04bd8
pau <sastrepau@gmail.com>
2012-05-06 20:29:04 +0200

    added the connection class

---
+++ P2P/P2P/Connection.h
@@ -0,0 +1,13 @@
+//
+//  Connection.h
+//  P2P
+//
+//  Created by Incomedia on 06/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Connection : NSObject
+
+@end
---
+++ P2P/P2P/Connection.m
@@ -0,0 +1,13 @@
+//
+//  Connection.m
+//  P2P
+//
+//  Created by Incomedia on 06/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Connection.h"
+
+@implementation Connection
+
+@end
