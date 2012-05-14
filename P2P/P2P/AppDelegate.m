a4076450413499f4367141a4e9488fea80ddf9bb
pau <sastrepau@gmail.com>
2012-05-14 13:10:27 +0200

    u

--- P2P/P2P/AppDelegate.m
+++ P2P/P2P/AppDelegate.m
@@ -92,11 +92,11 @@ volatile int32_t searchingThreadCount = 0;
                              andIpList:ipList 
                               withPath:localPath];
     
-    client = [Client newClientWithPort:localPort 
+    /*client = [Client newClientWithPort:localPort 
                              andIpList:ipList 
                               withPath:localPath 
                      withDownloadTable:_downloadsTable 
-                                 localConnection:TRUE];
+                                 localConnection:TRUE];*/
     
     [NSTimer scheduledTimerWithTimeInterval:10.0 
                                      target:client
